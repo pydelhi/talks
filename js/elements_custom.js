957c10ea42fc856c8526e78febef409a686de802
triptishukla <tripti12shukla1280@gmail.com>
2018-09-24 11:14:38 +0530

    deleted unneccesary files

---
+++ js/elements_custom.js
@@ -0,0 +1,455 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+
+1. Vars and Inits
+2. Set Header
+3. Initialize Hamburger
+4. Init Parallax
+5. Init Accordions
+6. Init Tabs
+7. Init Loaders
+8. Initialize Milestones
+9. Init SVG
+10. Init Search
+
+
+******************************/
+
+$(document).ready(function()
+{
+	"use strict";
+
+	/* 
+
+	1. Vars and Inits
+
+	*/
+
+	var header = $('.header');
+	var hamb = $('.hamburger');
+	var hambActive = false;
+	var menuActive = false;
+	var ctrl = new ScrollMagic.Controller();
+
+	setHeader();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+	});
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+	initHamburger();
+	initParallax();
+	initAccordions();
+	initTabs();
+	initLoaders();
+	initMilestones();
+	initSvg();
+	initSearch();
+
+	/* 
+
+	2. Set Header
+
+	*/
+
+	function setHeader()
+	{
+		if(window.innerWidth < 992)
+		{
+			if($(window).scrollTop() > 100)
+			{
+				header.addClass('scrolled');
+			}
+			else
+			{
+				header.removeClass('scrolled');
+			}
+		}
+		else
+		{
+			if($(window).scrollTop() > 100)
+			{
+				header.addClass('scrolled');
+			}
+			else
+			{
+				header.removeClass('scrolled');
+			}
+		}
+		if(window.innerWidth > 991 && menuActive)
+		{
+			closeMenu();
+		}
+	}
+
+	/* 
+
+	3. Initialize Hamburger
+
+	*/
+
+	function initHamburger()
+	{
+		if($('.hamburger').length)
+		{
+			hamb.on('click', function(event)
+			{
+				event.stopPropagation();
+
+				if(!menuActive)
+				{
+					openMenu();
+					
+					$(document).one('click', function cls(e)
+					{
+						if($(e.target).hasClass('menu_mm'))
+						{
+							$(document).one('click', cls);
+						}
+						else
+						{
+							closeMenu();
+						}
+					});
+				}
+				else
+				{
+					$('.menu_container').removeClass('active');
+					menuActive = false;
+				}
+			});
+		}
+	}
+
+	function openMenu()
+	{
+		var fs = $('.menu_container');
+		fs.addClass('active');
+		hambActive = true;
+		menuActive = true;
+	}
+
+	function closeMenu()
+	{
+		var fs = $('.menu_container');
+		fs.removeClass('active');
+		hambActive = false;
+		menuActive = false;
+	}
+
+	/* 
+
+	4. Init Parallax
+
+	*/
+
+	function initParallax()
+	{
+		// Add parallax effect to every element with class prlx
+		// Add class prlx_parent to the parent of the element
+		if($('.prlx_parent').length && $('.prlx').length)
+		{
+			var elements = $('.prlx_parent');
+
+			elements.each(function()
+			{
+				var ele = this;
+				var bcg = $(ele).find('.prlx');
+
+				var slideParallaxScene = new ScrollMagic.Scene({
+			        triggerElement: ele,
+			        triggerHook: 1,
+			        duration: "200%"
+			    })
+			    .setTween(TweenMax.from(bcg, 1, {y: '-30%', ease:Power0.easeNone}))
+			    .addTo(ctrl);
+			});
+		}
+	}
+
+	/* 
+
+	5. Init Accordions
+
+	*/
+
+	function initAccordions()
+	{
+		if($('.accordion').length)
+		{
+			var accs = $('.accordion');
+
+			accs.each(function()
+			{
+				var acc = $(this);
+
+				if(acc.hasClass('active'))
+				{
+					var panel = $(acc.next());
+					var panelH = panel.prop('scrollHeight') + "px";
+					
+					if(panel.css('max-height') == "0px")
+					{
+						panel.css('max-height', panel.prop('scrollHeight') + "px");
+					}
+					else
+					{
+						panel.css('max-height', "0px");
+					} 
+				}
+
+				acc.on('click', function()
+				{
+					if(acc.hasClass('active'))
+					{
+						acc.removeClass('active');
+						var panel = $(acc.next());
+						var panelH = panel.prop('scrollHeight') + "px";
+						
+						if(panel.css('max-height') == "0px")
+						{
+							panel.css('max-height', panel.prop('scrollHeight') + "px");
+						}
+						else
+						{
+							panel.css('max-height', "0px");
+						} 
+					}
+					else
+					{
+						acc.addClass('active');
+						var panel = $(acc.next());
+						var panelH = panel.prop('scrollHeight') + "px";
+						
+						if(panel.css('max-height') == "0px")
+						{
+							panel.css('max-height', panel.prop('scrollHeight') + "px");
+						}
+						else
+						{
+							panel.css('max-height', "0px");
+						} 
+					}
+				});
+			});
+		}
+	}
+
+	/* 
+
+	6. Init Tabs
+
+	*/
+
+	function initTabs()
+	{
+		if($('.tab').length)
+		{
+			$('.tab').on('click', function()
+			{
+				$('.tab').removeClass('active');
+				$(this).addClass('active');
+				var clickedIndex = $('.tab').index(this);
+
+				var panels = $('.tab_panel');
+				panels.removeClass('active');
+				$(panels[clickedIndex]).addClass('active');
+			});
+		}
+	}
+
+	/* 
+
+	7. Init Loaders
+
+	*/
+
+	function initLoaders()
+	{
+		if($('.loader').length)
+		{
+			var loaders = $('.loader');
+
+			loaders.each(function()
+			{
+				var loader = this;
+				var endValue = $(loader).data('perc');
+
+				var loaderScene = new ScrollMagic.Scene({
+		    		triggerElement: this,
+		    		triggerHook: 'onEnter',
+		    		reverse:false
+		    	})
+		    	.on('start', function()
+		    	{
+		    		var bar = new ProgressBar.Circle(loader,
+					{
+						color: '#fe3c52',
+						// This has to be the same size as the maximum width to
+						// prevent clipping
+						strokeWidth: 1.5,
+						trailWidth: 0,
+						easing: 'easeInOut',
+						duration: 1400,
+						text:
+						{
+							autoStyleContainer: false
+						},
+						from:{ color: '#fe3c52', width: 1.5 },
+						to: { color: '#fe3c52', width: 1.5 },
+						// Set default step function for all animate calls
+						step: function(state, circle)
+						{
+							circle.path.setAttribute('stroke', state.color);
+							circle.path.setAttribute('stroke-width', state.width);
+
+							var value = Math.round(circle.value() * 100);
+							if (value === 0)
+							{
+								circle.setText('0%');
+							}
+							else
+							{
+								circle.setText(value + "%");
+							}
+						}
+					});
+					bar.text.style.fontFamily = '"Montserrat", sans-serif';
+					bar.text.style.fontSize = '60px';
+					bar.text.style.fontWeight = '400';
+					bar.text.style.color = "#131a2f";
+
+
+					bar.animate(endValue);  // Number from 0.0 to 1.0
+		    	})
+			    .addTo(ctrl);
+			});
+		}
+	}
+
+	/* 
+
+	8. Initialize Milestones
+
+	*/
+
+	function initMilestones()
+	{
+		if($('.milestone_counter').length)
+		{
+			var milestoneItems = $('.milestone_counter');
+
+	    	milestoneItems.each(function(i)
+	    	{
+	    		var ele = $(this);
+	    		var endValue = ele.data('end-value');
+	    		var eleValue = ele.text();
+
+	    		/* Use data-sign-before and data-sign-after to add signs
+	    		infront or behind the counter number */
+	    		var signBefore = "";
+	    		var signAfter = "";
+
+	    		if(ele.attr('data-sign-before'))
+	    		{
+	    			signBefore = ele.attr('data-sign-before');
+	    		}
+
+	    		if(ele.attr('data-sign-after'))
+	    		{
+	    			signAfter = ele.attr('data-sign-after');
+	    		}
+
+	    		var milestoneScene = new ScrollMagic.Scene({
+		    		triggerElement: this,
+		    		triggerHook: 'onEnter',
+		    		reverse:false
+		    	})
+		    	.on('start', function()
+		    	{
+		    		var counter = {value:eleValue};
+		    		var counterTween = TweenMax.to(counter, 4,
+		    		{
+		    			value: endValue,
+		    			roundProps:"value", 
+						ease: Circ.easeOut, 
+						onUpdate:function()
+						{
+							document.getElementsByClassName('milestone_counter')[i].innerHTML = signBefore + counter.value + signAfter;
+						}
+		    		});
+		    	})
+			    .addTo(ctrl);
+	    	});
+		}
+	}
+
+	/* 
+
+	9. Init SVG
+
+	*/
+
+	function initSvg()
+	{
+		jQuery('img.svg').each(function()
+		{
+			var $img = jQuery(this);
+			var imgID = $img.attr('id');
+			var imgClass = $img.attr('class');
+			var imgURL = $img.attr('src');
+
+			jQuery.get(imgURL, function(data)
+			{
+				// Get the SVG tag, ignore the rest
+				var $svg = jQuery(data).find('svg');
+
+				// Add replaced image's ID to the new SVG
+				if(typeof imgID !== 'undefined') {
+				$svg = $svg.attr('id', imgID);
+				}
+				// Add replaced image's classes to the new SVG
+				if(typeof imgClass !== 'undefined') {
+				$svg = $svg.attr('class', imgClass+' replaced-svg');
+				}
+
+				// Remove any invalid XML tags as per http://validator.w3.org
+				$svg = $svg.removeAttr('xmlns:a');
+
+				// Replace image with new SVG
+				$img.replaceWith($svg);
+			}, 'xml');
+		});
+	}
+
+	/* 
+
+	10. Init Search
+
+	*/
+
+	function initSearch()
+	{
+		if($('.search').length)
+		{
+			var search = $('.search');
+			search.on('click', function(e)
+			{
+				var target = $(e.target);
+				if(!target.hasClass('ctrl_class'))
+				{
+					$(this).toggleClass('active');
+				}
+			});
+		}
+	}
+});
